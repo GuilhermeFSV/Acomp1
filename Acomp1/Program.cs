41670963e50ecc25714c3cb7506dcb27e7afeed3
Walber Antonio Ramos Beltrame <1020476@ifes.edu.br>
2019-08-09 16:08:09 -0300

    Versão para testes

--- Acomp1/Program.cs
+++ Acomp1/Program.cs
@@ -7,32 +7,25 @@ namespace Acomp1
     [MemoryDiagnoser]
     public class Tests
     {
-        //[Params(100, 1000, 10000)]
-        [Params(100)]
+        [Params(100, 1000, 10000)]
         public int N { get; set; }
 
-        //[Benchmark]
+        [Benchmark]
         public int TestMax1()
         {
             return (new Max1()).Max(new RandomArray(N).Array, N);
         }
 
-        //[Benchmark]
+        [Benchmark]
         public int TestMax2()
         {
             return (new Max2()).Max(new RandomArray(N).Array, N);
         }
 
-        //[Benchmark]
+        [Benchmark]
         public int TestMax3()
         {
             return (new Max3()).Max(new RandomArray(N).Array, N);
-        }
-
-        [Benchmark]
-        public int TestMax4()
-        {
-            return (new Max4()).Max(new RandomArray(N).Array, N);
         }
 
     }
